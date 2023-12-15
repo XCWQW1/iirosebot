23ac825c1fc71cab5975ef3488681c138180f3b8
XCWQW1 <3539757707@qq.com>
2023-12-15 15:22:31 +0800

    添加帮助菜单，插件相关指令仅限私聊使用

--- plugins/system_plugin_manage.py
+++ plugins/system_plugin_manage.py
@@ -1,21 +1,27 @@
-import asyncio
-
 from API.api_iirose import APIIirose
 from API.decorator.command import on_command, MessageType
 from API.api_message import send_markdown_code
 from API.api_get_config import get_master_id
-from plugin_system.plugin_init import plugin_manage_data
+from API.api_message import send_markdown_code
 from plugin_system.plugin_manage import *
 
 API = APIIirose()
 
 
-@on_command('.插件', substring=False, command_type=[MessageType.room_chat, MessageType.private_chat])
+@on_command('.插件', substring=False, command_type=[MessageType.private_chat])
 async def plugin_help(Message):
-    await API.send_msg(Message, '')
+    if Message.user_id == get_master_id():
+        await API.send_msg(Message, send_markdown_code('.插件 - 显示当前页面\n'
+                                                       '.插件 列表 - 列出所有插件\n'
+                                                       '.插件 启用 <插件名> - 启用插件\n'
+                                                       '.插件 禁用 <插件名> - 禁用插件\n'
+                                                       '.插件 重载 <插件名> - 重新载入插件\n'
+                                                       '.插件 加载 <插件名> - 加载未载入的插件'))
+    else:
+        await API.send_msg(Message, '无权执行')
 
 
-@on_command('.插件 ', substring=[True, 4], command_type=[MessageType.room_chat, MessageType.private_chat])
+@on_command('.插件 ', substring=[True, 4], command_type=[MessageType.private_chat])
 async def plugin_manage(Message, text):
     if Message.user_id == get_master_id():
         if text[:2] == '列表':
