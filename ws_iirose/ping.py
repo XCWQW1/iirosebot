ed63b712d60ea8411388e3efe2bbae5ed2ca0ec3
XCWQW1 <3539757707@qq.com>
2023-12-19 11:49:36 +0800

    新增股票信息获取，以及股票数据更新的函数调用

--- ws_iirose/ping.py
+++ ws_iirose/ping.py
@@ -5,6 +5,7 @@ async def ping_iirose(websocket):
     try:
         while True:
             await websocket.send('')
+            await websocket.send(">#")
             await asyncio.sleep(60)
     except:
         pass
