a0feffdee1725fff53937a4d9637231d58bfb146
XCWQW1 <3539757707@qq.com>
2024-02-19 11:33:32 +0800

    添加获取日志等级的函数

--- API/api_get_config.py
+++ API/api_get_config.py
@@ -27,3 +27,13 @@ def get_user_color() -> str:
         log.main.logger.warning('[警告|配置] 配置文件中的颜色不符合16进制')
         return 'DC143C'
 
+
+def get_log_level() -> str:
+    try:
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+        level = str(config["log"]["level"])
+    except:
+        level = 'INFO'
+
+    return level
