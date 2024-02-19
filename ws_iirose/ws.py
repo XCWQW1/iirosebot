d01544fe5b5a5c7198f88d8d52998050ea8e0efb
XCWQW1 <3539757707@qq.com>
2024-02-19 11:38:15 +0800

    优化重连ws地址

--- ws_iirose/ws.py
+++ ws_iirose/ws.py
@@ -25,7 +25,7 @@ async def connect_to_iirose_server():
     global bot_status
     global re_loop
     logger.info('正在连接')
-    wss_host = 2
+    wss_host = 1
     while True:
         try:
             async with websockets.connect(f'wss://m{wss_host if wss_host else ""}.iirose.com:8778') as websocket:
@@ -36,21 +36,16 @@ async def connect_to_iirose_server():
                 loop.create_task(login_to_server(websocket)), loop.create_task(ping_iirose(websocket))
                 async for message in websocket:
                     await process_message(message, websocket)
-        except Exception as e:
-            traceback.print_exc()
+        except:
             bot_status = Status.RECONNECT
             if GlobalVal.move_room:
                 logger.info(f'正在移动到 {GlobalVal.room_id}')
                 GlobalVal.move_room = False
             else:
-                logger.error(f'Error，ws连接断开：{e}，五秒后重连')
-                if re_loop:
-                    if wss_host != 9 and type(wss_host) == int:
-                        wss_host += 1
-                    elif wss_host is None:
-                        wss_host = 1
-                    else:
-                        wss_host = None
-                else:
-                    re_loop = True
+                logger.error(f'Error，ws连接断开：{traceback.format_exc()}，五秒后重连')
+                if wss_host == 2:
+                    wss_host = None
+                elif wss_host is None:
+                    wss_host = 1
+                wss_host = 2
                 time.sleep(5)
