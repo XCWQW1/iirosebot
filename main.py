1dd55199ea26e87af1ff20cbc342297180b566d7
XCWQW1 <3539757707@qq.com>
2024-06-25 05:23:24 +0800

    版本号更改为v1.6.2

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
     log_level = get_log_level()
     log(log_level)
-    iirosebot_version = 'v1.6.1'
+    iirosebot_version = 'v1.6.2'
 
     def check_version():
         try:
