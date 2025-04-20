ac819426a98c0ee6da482248bf2e1c8d6bff25be
xcwqw1 <xchome233@gmail.com>
2025-04-20 19:13:23 +0800

    输入内容为md5时跳过加密返回原始内容

--- iirosebot/utools/md5_encrypt.py
+++ iirosebot/utools/md5_encrypt.py
@@ -1,7 +1,11 @@
+import re
 import hashlib
 
 
 def md5_encrypt(string):
+    if bool(re.fullmatch(r"[a-fA-F0-9]{32}", string)):  # 已经是md5就不加密了
+        return string
+
     md5 = hashlib.md5()
     md5.update(string.encode('utf-8'))
     encrypted_string = md5.hexdigest()
